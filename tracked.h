d04ab7a673a0d21d6a0c31590c3614a8668d18a0
Carlo Wood <carlo@alinoe.com>
2017-04-28 18:14:02 +0200

    Refactored tracked.h
    This no longer supports derivation from a tracked class
    (ie 'D' is gone), nor the beginner stuff to show how
    things works regarding calling a member function, pre-
    post increments and derefences.
    
    However, it is now super easy create a new tracked class,
    derived from detail::tracked::Tracked<&name> with it's own
    name and easily exensible with it's own gadgets.

--- tracked.h
+++ tracked.h
@@ -1,154 +1,311 @@
-// Borrowed from http://eel.is/geordi/ which is Public Domain, yay!
-// Specifically, this is https://github.com/Eelis/geordi/blob/master/prelude/tracked.hpp
-// Several changes by Carlo Wood to make it more suitable for use in a project with libcwd.
+// Based on https://github.com/Eelis/geordi/blob/master/prelude/tracked.hpp
+// With major refactoring by Carlo Wood.
 
-#include <functional>
-#include <iostream>
+#pragma once
+
 #include <new>
+#include <iostream>
+#include <boost/range/adaptor/reversed.hpp>
+
+#define DECLARE_TRACKED(C)                                                      \
+    namespace { constexpr char const* const name_##C = #C; }                    \
+    struct C : tracked::detail::Tracked<&name_##C> {                            \
+      using tracked::detail::Tracked<&name_##C>::Tracked;                       \
+    }
 
 namespace tracked {
 namespace detail {
 
-class Tracked {
- protected:
-  Tracked();
-  Tracked(Tracked const&);
-#if __cplusplus >= 201103
-  Tracked(Tracked&&);
-#endif
-  ~Tracked();
+// Usage:
+//
+// DEFINE_TRACKED(B);
+//
+// after which using B will print debug info about
+// construction, destruction, copying, moving, assignment,
+// allocation and deletion to dc::tracked.
+//
+// One may use tracked::mute() and tracked::unmute() to
+// verbosely turn dc::tracked on and off.
 
-  void operator=(Tracked const&);
-#if __cplusplus >= 201103
-  void operator=(Tracked&&);
-#endif
-  void set_name(char const*) const;
-};
+template<char const* const* NAME>
+struct Tracked {
+  struct Entry {
+   private:
+    Tracked const* p;
 
-}  // namespace detail
+   public:
+    enum Status { fresh, pillaged, destructed, deleted } status;
+    Entry(Tracked const* t) : p(t), status(fresh) { }
 
-void mute();
-void unmute();
+    void set_status(Status st) { status = st; }
 
-namespace detail {
+    bool status_destructed() const { return status >= destructed; }
+    bool status_deleted() const { return status == deleted; }
+    bool status_below(Status st) const { return status < st; }
+    bool operator==(Tracked const* t) const { return p == t; }
+    bool operator<(void const* vp) const { return p < vp; }
 
-/* mute/unmute allow us to suppress boring messages that are not relevant to the issue under consideration.
- * Their use can be a bit tedious though. If we only want to get messages for a few statements inside a statement
- * block, we have to do something like:
- * geordi { mute(); ..foo.. unmute(); ..bar.. mute(); ..bas.. }
- * We therefore introduce some trickery to let us write the above as:
- * geordi { ..foo.. TRACK{ ..bar.. } ..bas.. }
- */
+    using Entries = std::vector<Entry>;
+    static Entries& entries();
+    std::ptrdiff_t id() const { return this - &entries().front(); }
 
-template <typename>
-class focus_t {
-  struct mute_in_ctor {
-    mute_in_ctor() { mute(); }
+    friend std::ostream& operator<<(std::ostream& os, Entry const& e) { return os << *NAME << e.id(); }
   };
-  static mute_in_ctor m;
 
- public:
-  focus_t()
+  Tracked()
   {
-    m;
-    unmute();
+    make_entry();
+    Dout(dc::tracked, *this << '*');
   }
-  // Mentioning m forces it to exist, but only if the focus_t template is ever instantiated.
-  // The effect is that if TRACK (which instantiates focus_t) is ever used, mute() will be called before main() is entered.
-  ~focus_t() { mute(); }
-  operator bool() const { return false; }
+
+  Tracked(Tracked const& lvalue)
+  {
+    lvalue.assert_status_below(Entry::pillaged, "copy");
+    make_entry();
+    Dout(dc::tracked, *this << "*(" << lvalue << ')');
+  }
+
+  Tracked(Tracked&& rvalue)
+  {
+    rvalue.assert_status_below(Entry::pillaged, "move");
+    make_entry();
+    Dout(dc::tracked, rvalue << "=>" << *this << '*');
+    rvalue.entry()->set_status(Entry::pillaged);
+  }
+
+  ~Tracked()
+  {
+    assert_status_below(Entry::destructed, "re-destruct");
+    Dout(dc::tracked, *this << '~');
+    entry()->set_status(Entry::destructed);
+  }
+
+  void operator=(Tracked const& r)
+  { 
+    assert_status_below(Entry::destructed, "assign to");
+    r.assert_status_below(Entry::pillaged, "assign from");
+    Dout(dc::tracked, *this << '=' << r);
+    entry()->set_status(Entry::fresh);
+  }
+
+  void operator=(Tracked&& r)
+  { 
+    assert_status_below(Entry::destructed, "move-assign to");
+    r.assert_status_below(Entry::pillaged, "move");
+    entry()->set_status(Entry::fresh);
+    Dout(dc::tracked, r << "=>" << *this);
+    r.entry()->set_status(Entry::pillaged);
+  }
+
+  void* operator new(std::size_t const s)
+  {
+    void* p = ::operator new(s);
+    return static_cast<Tracked*>(p)->op_new(s, false, p);
+  }
+
+  void* operator new[](std::size_t const s)
+  {
+    void* p = ::operator new[](s);
+    return static_cast<Tracked*>(p)->op_new(s, true, p);
+  }
+
+  void* operator new(std::size_t const s, std::nothrow_t const& t) throw()
+  {
+    void* p = ::operator new(s, t);
+    return static_cast<Tracked*>(p)->op_new(s, false, p);
+  }
+
+  void* operator new[](std::size_t const s, std::nothrow_t const& t) throw()
+  {
+    void* p = ::operator new[](s, t);
+    return static_cast<Tracked*>(p)->op_new(s, true, p);
+  }
+
+  void* operator new(std::size_t const, void* const p) throw() { return p; }
+  void* operator new[](std::size_t const, void* const p) throw() { return p; }
+
+  void operator delete(void* const p, std::size_t const s) throw()
+  { 
+    static_cast<Tracked*>(p)->op_delete(static_cast<Tracked*>(p), s);
+  }
+
+  void operator delete[](void* const p, std::size_t const s) throw()
+  { 
+    static_cast<Tracked*>(p)->op_array_delete(static_cast<Tracked*>(p), s);
+  }
+
+  void make_entry() const;
+  Entry* entry() const;
+
+  static void atexit();
+
+  void assert_status_below(typename Entry::Status status, std::string const& s) const;
+  void* op_new(std::size_t, bool const array, void* const r);
+  void op_delete(void* const p, std::size_t const s);
+  void op_array_delete(void* const p, std::size_t const s);
+
+  friend std::ostream& operator<<(std::ostream& os, Tracked const& t) { return os << *t.entry(); }
 };
 
-template <typename T>
-typename focus_t<T>::mute_in_ctor focus_t<T>::m;
+//static
+template<char const* const* NAME>
+typename Tracked<NAME>::Entry::Entries& Tracked<NAME>::Entry::entries()
+{
+  static Entries* p = nullptr;
+  if (!p)
+  {
+    p = new Entries;
+    std::atexit(&atexit);
+  }
+  return *p;
+}
+
+template<char const* const* NAME>
+void Tracked<NAME>::make_entry() const
+{
+  if (Entry* const e = entry())
+    if (!e->status_destructed())
+      Dout(dc::tracked, "leaked: " << e << '.');
+  Entry::entries().emplace_back(this);
+}
+
+template<char const* const* NAME>
+typename Tracked<NAME>::Entry* Tracked<NAME>::entry() const
+{
+  for (auto&& e : boost::adaptors::reverse(Entry::entries()))
+    if (e == this) return &e;
+  return nullptr;
+}
+
+template<char const* const* NAME>
+void Tracked<NAME>::assert_status_below(typename Entry::Status status, std::string const& s) const
+{
+  Entry* const e = entry();
+  if (!e)
+    Dout(dc::tracked, "Trying to " << s << " non-existent object:");
+  if (e->status_below(status))
+    return;
+  Dout(dc::tracked, "Trying to " << s << (e->status_destructed() ? " destructed " : " pillaged ") << *e << ':');
+}
+
+template<char const* const* NAME>
+void* Tracked<NAME>::op_new(std::size_t, bool const array, void* const r)
+{ 
+  if (!r)
+    return 0;
+  Dout(dc::tracked, "new(" << *NAME << (array ? "[]" : "") << ")");
+  return r;
+}
+
+template<char const* const* NAME>
+void Tracked<NAME>::op_delete(void* const p, std::size_t const s)
+{ 
+  ::operator delete(p);
+  
+  for (auto&& e : Entry::entries())
+    if (!(e < p) && e < static_cast<char*>(p) + s)
+    {
+      Dout(dc::tracked, "delete(" << e << ")");
+      e.set_status(Entry::deleted);
+      return;
+    }
+}
+
+template<char const* const* NAME>
+void Tracked<NAME>::op_array_delete(void* const p, std::size_t const s)
+{
+  ::operator delete[](p);
+  Dout(dc::tracked|continued_cf, "delete[");
+  bool first = true;
+  for (auto&& e : Entry::entries())
+    if (!e.status_deleted() && !(e < p) && e < static_cast<char*>(p) + s)
+    {
+      if (first)
+        first = false;
+      else
+        Dout(dc::continued, ", ");
+      Dout(dc::continued, e);
+      e.set_status(Entry::deleted);
+    }
+  Dout(dc::finish, ']');
+}
+
+//static
+template<char const* const* NAME>
+void Tracked<NAME>::atexit()
+{
+  bool first = true;
+  for (auto&& e : Entry::entries())
+    if (!e.status_destructed())
+    {
+      if (first)
+      {
+        Dout(dc::tracked|continued_cf, "leaked: ");
+        first = false;
+      }
+      else
+        Dout(dc::continued, ", ");
+
+      Dout(dc::continued, e);
+    }
+
+  if (!first)
+  {
+    Dout(dc::finish, ".");
+  }
+}
 
 } // namespace detail
 
-#define TRACK                                                                                            \
-  if (::tracked::detail::focus_t<void> const& tracked_detail_focus = ::tracked::detail::focus_t<void>()) \
-    ;                                                                                                    \
-  else
+void mute()
+{ 
+  Dout(dc::tracked, "muted");
+  Debug(dc::tracked.off());
+}
 
-struct B : protected detail::Tracked {
-  B();
-  B(B const&);
-#if __cplusplus >= 201103
-  B(B &&);
+void unmute()
+{ 
+  Debug(dc::tracked.on());
+  Dout(dc::tracked, "unmuted");
+}
+
+} // namespace tracked;
+
+#if 0
+DECLARE_TRACKED(B);
+
+int main()
+{
+  Debug(NAMESPACE_DEBUG::init());
+
+  B b0;                        // B0*
+  B* d;
+  {
+    B b1;                      // B1*
+    B b2(b1);                  // B2*(B1)
+    B b3(b1);                  // B3*(B1)
+    B b4(std::move(b1));       // B1=>B4*
+    B b5(b1);                  // Trying to copy pillaged B1: B5*(B1)
+    B b6(std::move(b1));       // Trying to move pillaged B1: B1=>B6*                    
+    b4.~B();                   // B4~
+    d = &b1;
+  }                            // B6~, B5~, Trying to re-destruct destructed B4: B4~, B3~, B2~, B1~
+
+  B b7(*d);                    // Trying to copy destructed B1: B7*(B1)
+
+  b7 = b0;                     // B7=B0
+  b7 = *d;                     // Trying to assign from destructed B1: B7=B1
+  b7 = std::move(b0);          // B0=>B7
+  b7 = b0;                     // Trying to assign from pillaged B0: B7=B0
+
+  B b8;                        // B8*
+  Dout(dc::notice, "b8 = " << b8); // b8 = B8
+
+  B* b9 = new B;               // new(B), B9*
+  B* b10 = new B(*b9);         // new(B), B10*(B9)
+  delete b9;                   // B9~, delete(B9)
+  delete b10;                  // B10~, delete(B10)
+  B* b11 = new B[3];           // new(B[]), B11*, B12*, B13*
+  delete [] b11;               // B13~, B12~, B11~, delete[B11, B12, B13]
+}                              // B8~, B7~, B0~
 #endif
-
-  B& operator=(B const&);
-#if __cplusplus >= 201103
-  B& operator=(B&&);
-#endif
-  virtual ~B();
-
-  void* operator new(std::size_t);
-  void* operator new[](std::size_t);
-#if __cplusplus >= 201103
-  void* operator new(std::size_t, std::nothrow_t const&) throw();
-  void* operator new[](std::size_t, std::nothrow_t const&) throw();
-#endif
-  void* operator new(std::size_t const, void* const p) throw() { return p; }
-  void* operator new[](std::size_t const, void* const p) throw() { return p; }
-  void operator delete(void*, std::size_t) throw();
-  void operator delete[](void*, std::size_t) throw();
-
-  void f() const;
-  virtual void vf() const;
-
-  B& operator++();
-  B operator++(int);
-
-  void operator*() const;
-
-  template <typename C, typename Tr>
-  friend std::basic_ostream<C, Tr>& operator<<(std::basic_ostream<C, Tr>&, B const&);
-
- private:
-  template <typename C, typename Tr>
-  void print(std::basic_ostream<C, Tr>&) const;
-};
-
-bool operator<(B const&, B const&);
-bool operator==(B const&, B const&);
-inline bool operator<=(B const& x, B const& y) { return x == y || x < y; }
-inline bool operator>(B const &x, B const &y) { return y < x; }
-inline bool operator>=(B const &x, B const &y) { return y <= x; }
-inline bool operator!=(B const &x, B const &y) { return !(x == y); }
-
-struct D : B {
-  D();
-  D(D const&);
-#if __cplusplus >= 201103
-  D(D&&);
-#endif
-
-  D& operator=(D const&);
-#if __cplusplus >= 201103
-  D& operator=(D&&);
-#endif
-  ~D();
-
-  void* operator new(std::size_t);
-  void* operator new[](std::size_t);
-#if __cplusplus >= 201103
-  void* operator new(std::size_t, std::nothrow_t const&) throw();
-  void* operator new[](std::size_t, std::nothrow_t const&) throw();
-#endif
-  void* operator new(std::size_t const, void* const p) throw() { return p; }
-  void* operator new[](std::size_t const, void* const p) throw() { return p; }
-  void operator delete(void*, std::size_t) throw();
-  void operator delete[](void*, std::size_t) throw();
-
-  void f() const;
-  virtual void vf() const;
-
-  template <typename C, typename Tr>
-  friend std::basic_ostream<C, Tr>& operator<<(std::basic_ostream<C, Tr>&, D const&);
-
- private:
-  template <typename C, typename Tr>
-  void print(std::basic_ostream<C, Tr>&) const;
-};
-
-} // namespace tracked
