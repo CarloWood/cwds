ada4a8fa6f2a7c30f714d53cb982720d17939816
Carlo Wood <carlo@alinoe.com>
2019-12-02 21:07:13 +0100

    rewrite_header: add module name and desc.

--- benchmark.h
+++ benchmark.h
@@ -1,21 +1,28 @@
 /**
+ * cwds -- Application-side libcwd support code.
+ *
  * @file
  * @brief This file contains the declarations of namespace benchmark.
  *
  * @Copyright (C) 2018  Carlo Wood.
  *
- * This program is free software: you can redistribute it and/or modify
+ * RSA-1024 0x624ACAD5 1997-01-26                    Sign & Encrypt
+ * Fingerprint16 = 32 EC A7 B6 AC DB 65 A6  F6 F6 55 DD 1C DC FF 61
+ *
+ * This file is part of cwds.
+ *
+ * Cwds is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * Cwds is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with cwds.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
