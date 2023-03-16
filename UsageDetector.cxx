bb892a38010038afae30aa65afbaf7b2bdbf19df
Carlo Wood <carlo@alinoe.com>
2023-03-16 14:29:15 +0100

    Add support for clang++ 15.x.

--- UsageDetector.cxx
+++ UsageDetector.cxx
@@ -1,8 +1,13 @@
 #include "sys.h"
 #include "debug.h"
 
+// UsageDetector requires -std=c++2b or higher.
+#if __cplusplus >= 202101L
+
 #ifdef CWDEBUG
 NAMESPACE_DEBUG_CHANNELS_START
 channel_ct usage_detector("USAGE");
 NAMESPACE_DEBUG_CHANNELS_END
+#endif
+
 #endif
